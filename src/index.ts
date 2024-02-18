87bc9fbd52264714323f28cac79fc806ecb9dacd
Abhay Panchal <abpanchal951357@gmail.com>
2024-02-18 18:25:37 +0530

    chore: cors error trying to resolve

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,13 @@ import { getAllSlots } from "./controllers";
 import cors from "cors";
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  })
+);
 const PORT = 3000;
 
 // middleware
