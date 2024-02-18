204df50d6efe5ad8ed62a717d1241cc80a24c858
Abhay Panchal <abpanchal951357@gmail.com>
2024-02-18 17:43:52 +0530

    chore:minor get command added

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import {router as patentRouter} from "./routes/patient";
-import {router as adminRouter} from "./routes/admin";
-import {router as physioRouter} from "./routes/physio";
+import { router as patentRouter } from "./routes/patient";
+import { router as adminRouter } from "./routes/admin";
+import { router as physioRouter } from "./routes/physio";
 import { getAllSlots } from "./controllers";
 
 const app = express();
@@ -19,7 +19,11 @@ app.use("/api/patient", patentRouter);
 app.use("/api/admin", adminRouter);
 app.use("/api/physio", physioRouter);
 
-app.use('/api/slots', getAllSlots)
+app.get("/", (req, res) => {
+  res.send("<h1>Hello From Abhay</h1>");
+});
+
+app.use("/api/slots", getAllSlots);
 
 app.listen(PORT, () => {
   console.log(`Server Started at port ${PORT}`);
