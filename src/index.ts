d28827e2fcb26481658cd21a3997ea8943895043
Abhay Panchal <abpanchal951357@gmail.com>
2024-02-18 18:28:27 +0530

    chore: cors error trying to resolve

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,18 @@ import { getAllSlots } from "./controllers";
 import cors from "cors";
 
 const app = express();
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
-  })
-);
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 const PORT = 3000;
 
 // middleware
