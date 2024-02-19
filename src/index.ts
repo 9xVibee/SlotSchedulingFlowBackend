41de17cf55322050a808287683ba87fad421243a
Abhay Panchal <abpanchal951357@gmail.com>
2024-02-19 09:40:23 +0530

    chore: allowed cors for everyone

--- src/index.ts
+++ src/index.ts
@@ -7,17 +7,7 @@ import { getAllSlots } from "./controllers";
 import cors from "cors";
 
 const app = express();
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
+app.use(cors());
 
 const PORT = 3000;
 
